4796d2904f3eb433ebac16adba3ce21eb3cf4652
Asyrantheyt <94661066+Asyrantheyt@users.noreply.github.com>
2023-05-20 18:28:52 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,7 +7,7 @@ local okButton = Instance.new("TextButton")
 local okButton2 = Instance.new("TextButton")
 
 test.Name = "test"
-test.Parent = game:GetService("CoreGui")
+test.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 test.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 Frame.Parent = test
@@ -16,9 +16,6 @@ Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Frame.BorderSizePixel = 0
 Frame.Position = UDim2.new(0.528843105, 0, 0.499180138, 0)
 Frame.Size = UDim2.new(0.343765229, 0, 0.391803265, 0)
-Frame.Draggable = true
-Frame.Selectable = true
-Frame.Active = true
 
 UICorner.CornerRadius = UDim.new(0, 10)
 UICorner.Parent = Frame
@@ -27,7 +24,7 @@ ScrollingFrame.Parent = Frame
 ScrollingFrame.Active = true
 ScrollingFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 ScrollingFrame.Position = UDim2.new(0.0211922526, 0, 0.0334727764, 0)
-ScrollingFrame.Size = UDim2.new(0, 424, 0, 181)
+ScrollingFrame.Size = UDim2.new(0.961, 0,0.757, 0)
 ScrollingFrame.CanvasSize = UDim2.new(0, 0, 1, 0)
 
 TextLabel.Parent = ScrollingFrame
@@ -48,7 +45,7 @@ okButton.Parent = Frame
 okButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 okButton.BorderSizePixel = 0
 okButton.Position = UDim2.new(0.0201342404, 0, 0.820083678, 0)
-okButton.Size = UDim2.new(0, 199, 0, 43)
+okButton.Size = UDim2.new(0.451, 0,0.18, 0)
 okButton.Font = Enum.Font.SourceSans
 okButton.Text = "load teammcheck version"
 okButton.TextColor3 = Color3.fromRGB(0, 0, 0)
@@ -61,14 +58,53 @@ okButton2.Parent = Frame
 okButton2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 okButton2.BorderSizePixel = 0
 okButton2.Position = UDim2.new(0.523489952, 0, 0.820083678, 0)
-okButton2.Size = UDim2.new(0, 199, 0, 43)
+okButton2.Size = UDim2.new(0.451, 0,0.18, 0)
 okButton2.Font = Enum.Font.SourceSans
 okButton2.Text = "load no teammcheck version"
 okButton2.TextColor3 = Color3.fromRGB(0, 0, 0)
 okButton2.TextScaled = true
 okButton2.TextSize = 14.000
 okButton2.TextWrapped = true
-
+local function KADM_fake_script()
+	local script = Instance.new("LocalScript", Frame)
+	
+	local uis = game:GetService("UserInputService")
+	local ts = game:GetService("TweenService")
+	local dragging
+	local draginput
+	local dragstart
+	local startpos
+	local gui = Frame
+	
+	local function update(input : Enums)
+		local delta = input.Position - dragstart
+		ts:Create(gui,TweenInfo.new(0.25),{["Position"] = UDim2.new(startpos.X.Scale, startpos.X.Offset + delta.X, startpos.Y.Scale, startpos.Y.Offset + delta.Y)}):Play()
+	end
+	
+	gui.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or Enum.UserInputType.Touch) then
+			dragging = true
+			dragstart = input.Position
+			startpos = gui.Position
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+	end)
+	gui.InputChanged:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseMovement or Enum.UserInputType.Touch) then
+			draginput = input
+		end
+	end)
+	uis.InputChanged:Connect(function(input)
+		if input == draginput and dragging then
+			update(input)
+		end
+	end)
+end
+coroutine.wrap(KADM_fake_script)()
 local function ZILTY_fake_script() -- TextLabel.LocalScript 
 	local script = Instance.new('LocalScript', TextLabel)
 	script.Parent.Text = "Dear, "..tostring(game:GetService("Players").LocalPlayer.DisplayName).." this script is not that secure so pls be cool using this script."
@@ -76,16 +112,17 @@ end
 coroutine.wrap(ZILTY_fake_script)()
 local function FSNE_fake_script() -- nil. 
 	local script = Instance.new('LocalScript', okButton)
-	script.Parent.MouseButton1Click:Connect(function()
-	script.Parent.Parent.Parent.Enabled = false;
-	local ok, sus = pcall(function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Asyrantheyt/aimbot_for_mobile/main/aimbot", true))();
-	end);
-	if sus then
-		print("Studio cannot load 'https://raw.githubusercontent.com/Asyrantheyt/aimbot_for_mobile/main/aimbot'");
-		warn(sus);
-		end
-	end);
+	script.Parent.MouseButton1Click:Connect(function() 
+		script.Parent.Parent.Parent.Enabled = false 
+		local ok, sus = pcall(function() 
+			loadstring(game:HttpGet(("https://raw.githubusercontent.com/Asyrantheyt/aimbot_for_mobile/main/aimbot"),true))() 
+		end) 
+		if sus 
+		then 
+			print("Studio cannot load 'https://raw.githubusercontent.com/Asyrantheyt/aimbot_for_mobile/main/aimbot'") 
+			warn(sus) 
+		end 
+	end)
 end
 coroutine.wrap(FSNE_fake_script)()
 local function GHSA_fake_script()
